5a6c1491475fa20b1dcbf2d038b6af2224cf6d07
emmamkhoyan25 <you@example.commkhoyanemma2010@gmail.com>
2025-08-22 18:20:12 +0400

    fixed welcome name

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import  "./App.scss"
+import "./App.scss"
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Welcome from './pages/welcome/Welcome'
+import Welcome from './pages/Welcome/Welcome'
 import Order from './pages/order/Order'
 import Home from './pages/home/Home'
 import Detail from './pages/Detail/Detail'
 const App = () => {
   return (
-   <BrowserRouter>
-   
-   <Routes>
-    <Route path='/' element={<Welcome/>}/>
-    <Route path='/home' element={<Home/>}/>
-    <Route path='/detail/:i' element={<Detail/>}/>
-     <Route path='/order' element={<Order/>}/>
-   </Routes>
-   
-   </BrowserRouter>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/detail/:i' element={<Detail />} />
+        <Route path='/order' element={<Order />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -3,15 +3,15 @@ import styles from "./Welcome.module.scss"
 import { useNavigate } from 'react-router'
 
 const Welcome = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     return (
         <div className="container">
             <div className={styles.welcome}>
-                <img src="/coffee.png"  />
+                <img src="/coffee.png" />
                 <div>
-                <h2>Fall in Love with Coffee in Blissful Delight!</h2>
-                <p>Welcome to our cozy coffee corner, where every cup is a delightful for you.</p>
-                <button onClick={()=> {navigate("/home")}}>Get Started</button>
+                    <h2>Fall in Love with Coffee in Blissful Delight!</h2>
+                    <p>Welcome to our cozy coffee corner, where every cup is a delightful for you.</p>
+                    <button onClick={() => { navigate("/home") }}>Get Started</button>
                 </div>
             </div>
         </div>
