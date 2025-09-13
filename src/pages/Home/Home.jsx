2a2001e98dc1a2a40afbac984cb3506ca0ddf350
emmamkhoyan25 <you@example.commkhoyanemma2010@gmail.com>
2025-09-13 15:14:26 +0400

    changes home/welcome.scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,45 @@
 import React, { useState } from 'react'
-import { coffees } from '../../datas/Coffees';
-import styles from "./Home.module.scss"
-import CoffeeCard from '../../components/CoffeeCard/CoffeeCard';
-
+import { coffees } from '../../datas/Coffees'
+import styles from './Home.module.scss'
+import CoffeeCard from '../../components/CoffeeCard/CoffeeCard'
 
 const Home = () => {
+  const [search, setSearch] = useState('')
+  const [coffeesArr, setCoffeesArr] = useState(coffees)
 
-    const [search, setSearch] = useState("");
-    const [coffeesArr, setCoffeesArr] = useState(coffees);
-    return (
-        <div className={"container " + styles.home}>
-            <input type="text" value={search}
-                onChange={e => setSearch(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.code == "Enter") {
-                        setCoffeesArr(coffees.filter(coffee => coffee.name.toLowerCase().includes(search.toLowerCase())))
-                    }
-                }} />
-            <div className={styles.coffee_grid}>
-               
-                {coffeesArr.length ? coffeesArr.map((coffee, i) =>
-                 <CoffeeCard key={i} coffeeInfo={coffee} index={i} />):"We dont have such an assortement"}
-            </div>
-
-        </div>
+  const handleSearch = () => {
+    const filtered = coffees.filter((coffee) =>
+      coffee.name.toLowerCase().includes(search.toLowerCase())
     )
+    setCoffeesArr(filtered)
+  }
+
+  return (
+    <div className={`container ${styles.home}`}>
+      <div className={styles.inputWrapper}>
+        <input
+          type="text"
+          value={search}
+          placeholder="Search coffee... ⏎"
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.code === 'Enter') handleSearch()
+          }}
+        />
+        <span className={styles.enterIcon}>⏎</span>
+      </div>
+
+      <div className={styles.coffee_grid}>
+        {coffeesArr.length > 0 ? (
+          coffeesArr.map((coffee, i) => (
+            <CoffeeCard key={i} coffeeInfo={coffee} index={i} />
+          ))
+        ) : (
+          <p className={styles.noResult}>We don't have such an assortment.</p>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Home
